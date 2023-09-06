0e7d8bedda6105ce541d05e9d80b2d806654f998
Nam <namdeveloper.ca@gmail.com>
2023-09-06 11:43:49 +0700

    Finish project

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -24,7 +24,7 @@ export const options: NextAuthOptions = {
         //   headers: { 'Content-Type': 'application/json' },
         // })
         // const user = await res.json()
-        const user = { id: '64', name: 'namkhanh', password: 'namkhanh' }
+        const user = { id: '64', name: 'namkhanh', password: 'namkhanh', email: 'test@gmail.com' }
 
         // If no error and we have user data, return it
         // if (res.ok && user) {
